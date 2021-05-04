81f4ff75c78ff7a3f0feb94e9a930462b469307e
fatzergling <andrew@zdhome.net>
2021-05-04 01:03:58 -0400

    add search api

---
+++ search-server/src/main/java/edu/upenn/cis/cis455/Url.java
@@ -0,0 +1,13 @@
+package edu.upenn.cis.cis455;
+
+class Url {
+	private String url;
+	
+	public String getUrl() {
+		return url;
+	}
+	
+	public void setUrl(String url) {
+		this.url = url;
+	}
+}
---
+++ search-server/src/main/java/edu/upenn/cis/cis455/db.java
@@ -0,0 +1,35 @@
+package edu.upenn.cis.cis455;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+public class db {
+    static Logger logger = LogManager.getLogger(db.class);
+
+	public db() {
+		
+	}
+	
+	static Connection getRemoteConnection() {
+	      try {
+	      Class.forName("org.postgresql.Driver");
+	      String dbName = "postgres";
+	      String userName = "master";
+	      String password = "ilovezackives";
+	      String hostname = "cis555-project.ckm3s06jrxk1.us-east-1.rds.amazonaws.com";
+	      String port = "5432";
+	      String jdbcUrl = "jdbc:postgresql://" + hostname + ":" + port + "/" + dbName + "?user=" + userName + "&password=" + password;
+	      Connection con = DriverManager.getConnection(jdbcUrl);
+	      System.out.println("Remote connection successful.");
+	      return con;
+	    }
+	    catch (ClassNotFoundException e) { logger.warn(e.toString());}
+	    catch (SQLException e) { logger.warn(e.toString());}
+
+	     System.out.println("Remote connection unsuccessful.");
+	    return null;
+	  }
+
+}
